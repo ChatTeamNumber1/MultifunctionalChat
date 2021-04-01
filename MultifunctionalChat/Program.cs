48ed35d82d702215b02cc82704c960c8c012fbcb
mabramovsd <beavisabra@yandex.ru>
2021-04-01 11:49:42 +0400

    Удалил лишние файлы, связь сообщений и пользователей

--- MultifunctionalChat/Program.cs
+++ MultifunctionalChat/Program.cs
@@ -21,6 +21,7 @@ namespace MultifunctionalChat
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseWebRoot("wwwroot");
                 });
     }
 }
