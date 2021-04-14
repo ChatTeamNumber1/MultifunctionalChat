7366c37899c140b3be18c000c6ef8e396aaf34f5
mabramovsd <beavisabra@yandex.ru>
2021-04-14 11:23:10 +0400

    Парсинг сообщения на сервере, переименование комнаты: начало

--- MultifunctionalChat/Controllers/MessageController.cs
+++ MultifunctionalChat/Controllers/MessageController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MultifunctionalChat.Models;
@@ -10,11 +12,13 @@ namespace MultifunctionalChat.Controllers
     public class MessageController : ControllerBase
     {
         private readonly IRepository<Message> messageService;
+        private readonly IRepository<Room> roomService;
         private readonly ILogger<MessageController> logger;
 
-        public MessageController(IRepository<Message> messageService, ILogger<MessageController> logger)
+        public MessageController(IRepository<Message> messageService, IRepository<Room> roomService, ILogger<MessageController> logger)
         {
             this.messageService = messageService;
+            this.roomService = roomService;
             this.logger = logger;
         }
 
@@ -37,9 +41,67 @@ namespace MultifunctionalChat.Controllers
         [HttpPost]
         public ActionResult<Message> Post(Message message)
         {
-            messageService.Create(message);
-            logger.LogInformation($"Сообщение с id = {message.Id} добавлено в общий список");
-            return Ok($"Сообщение с id = {message.Id} добавлено в общий список");
+            string result = "";
+
+            //Обычные сообщения
+            if (!message.Text.StartsWith("//"))
+            {
+                messageService.Create(message);
+                result = $"Сообщение с id = {message.Id} добавлено в общий список";
+            }
+            //Команды
+            else
+            {
+                string[] messageParts = message.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (messageParts[0] == "//room")
+                {
+                    if (messageParts.Length > 1 && messageParts[1] == "rename")
+                    {
+                        //Тут через пробел идут названия двух комнат. 
+                        //Извращения вызваны тем, что названия и сами могут содержать пробелы
+                        string trimmedMessage = message.Text.Replace("//room", "").Replace("rename", "").Trim();
+                        string[] renamedParts = trimmedMessage.Split(new string[] { "||" }, StringSplitOptions.RemoveEmptyEntries);
+                        
+                        //TODO Проверку ошибок бы всю в одно место, а то разбросало...
+                        if (renamedParts.Length < 2)
+                        {
+                            result = "Неверный формат сообщения (не найдены ||)";
+                            logger.LogInformation(result);
+                            return Ok(result);
+                        }
+
+                        var roomToRename = roomService.GetList().Where(
+                            room => room.Name == renamedParts[0].Trim()).FirstOrDefault();
+                        
+                        if (roomToRename == null)
+                        {
+                            result = $"Неверное название комнаты";
+                            logger.LogInformation(result);
+                            return Ok(result);
+                        }
+                        //TODO Тут бы еще и на администратора проверить
+                        else if (message.UserId != roomToRename.OwnerId)
+                        {
+                            result = $"Недостаточно прав для переименования комнаты с id = {roomToRename.Id}";
+                            logger.LogInformation(result);
+                            return Ok(result);
+                        }
+
+                        string newRoomName = renamedParts[1].Trim();
+                        roomToRename.Name = newRoomName;
+                        roomService.Update(roomToRename);
+                        result = $"Обновлены данные о комнате с id = {roomToRename.Id}";                        
+                    }
+                }
+                else
+                {
+                    result = $"Сделано что-то другое";
+                }
+            }
+
+            logger.LogInformation(result);
+            return Ok(result);
         }
 
         [HttpPut("{id}")]
--- MultifunctionalChat/StaticVars.cs
+++ MultifunctionalChat/StaticVars.cs
@@ -11,5 +11,13 @@ namespace MultifunctionalChat
         /// Самая общая комната
         /// </summary>
         public static string DEFAULT_ROOM_ID = "3";
+        /// <summary>
+        /// Администратор
+        /// </summary>
+        public static string ROLE_ADMIN = "1";
+        /// <summary>
+        /// Модератор
+        /// </summary>
+        public static string ROLE_MODERATOR = "2";
     }
 }
